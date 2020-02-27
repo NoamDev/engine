38593f213f86b0e7b42234e920f836b65675a17a
NoamDev <37066741+NoamDev@users.noreply.github.com>
2020-02-27 19:55:00 +0200

    Revert "Eliminate unused import in Android embedding (#15975)"
    This reverts commit 2e0fc4516413fae330011696ca49e15ac68fe932.

--- shell/platform/android/io/flutter/embedding/android/FlutterFragment.java
+++ shell/platform/android/io/flutter/embedding/android/FlutterFragment.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.arch.lifecycle.Lifecycle;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
