bcbc98901d2889f0a3c38aee1b2b352e4f3cfb82
NoamDev <37066741+NoamDev@users.noreply.github.com>
2020-02-27 19:55:00 +0200

    Revert "Implement Path.from in the CanvasKit backend (#14468)"
    This reverts commit cae189a433080d29555f4d0b4db4e875c7f9b26d.

--- lib/web_ui/lib/src/engine/compositor/path.dart
+++ lib/web_ui/lib/src/engine/compositor/path.dart
@@ -15,9 +15,9 @@ class SkPath implements ui.Path {
     fillType = ui.PathFillType.nonZero;
   }
 
+  // TODO(yjbanov): implement: https://github.com/flutter/flutter/issues/46812
   SkPath.from(SkPath other) {
-    _skPath = js.JsObject(canvasKit['SkPath'], <js.JsObject>[other._skPath]);
-    fillType = other.fillType;
+    throw UnimplementedError('SkPath.from is not implemented in the CanvasKit backend');
   }
 
   SkPath._fromSkPath(js.JsObject skPath) : _skPath = skPath;
