a5be8d193768c71ebecde7c22405056617f583f9
NoamDev <37066741+NoamDev@users.noreply.github.com>
2020-02-27 19:55:00 +0200

    Revert "Disable flakey CountDownLatchTest.CanWait unit test on Fuchsia (#15982)"
    This reverts commit b0295a722f82ef8891901beeffcb12b02fd28f9f.

--- fml/synchronization/count_down_latch_unittests.cc
+++ fml/synchronization/count_down_latch_unittests.cc
@@ -5,7 +5,6 @@
 #include <chrono>
 #include <thread>
 
-#include "flutter/fml/build_config.h"
 #include "flutter/fml/synchronization/count_down_latch.h"
 #include "flutter/fml/thread.h"
 #include "flutter/testing/testing.h"
@@ -17,11 +16,6 @@ TEST(CountDownLatchTest, CanWaitOnZero) {
   latch.Wait();
 }
 
-#if OS_FUCHSIA
-#define CanWait DISABLED_CanWait
-#else
-#define CanWait CanWait
-#endif
 TEST(CountDownLatchTest, CanWait) {
   fml::Thread thread("test_thread");
   const size_t count = 100;
