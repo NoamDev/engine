4dd6eb8df676180988a923e58d676d8c83b77629
NoamDev <37066741+NoamDev@users.noreply.github.com>
2020-02-27 19:55:00 +0200

    Revert "More efficient decoding for down-sampled Flutter images using cache(Width|Height) (#15372)"
    This reverts commit 4e8a88df5f056ae03a4c33f3a5a51b962727e84d.

--- lib/ui/painting/image_decoder.h
+++ lib/ui/painting/image_decoder.h
@@ -13,7 +13,6 @@
 #include "flutter/fml/concurrent_message_loop.h"
 #include "flutter/fml/macros.h"
 #include "flutter/fml/mapping.h"
-#include "flutter/fml/trace_event.h"
 #include "flutter/lib/ui/io_manager.h"
 #include "third_party/skia/include/core/SkData.h"
 #include "third_party/skia/include/core/SkImage.h"
@@ -68,11 +67,6 @@ class ImageDecoder {
 
   FML_DISALLOW_COPY_AND_ASSIGN(ImageDecoder);
 };
-
-sk_sp<SkImage> ImageFromCompressedData(sk_sp<SkData> data,
-                                       std::optional<uint32_t> target_width,
-                                       std::optional<uint32_t> target_height,
-                                       const fml::tracing::TraceFlow& flow);
 
 }  // namespace flutter
 
