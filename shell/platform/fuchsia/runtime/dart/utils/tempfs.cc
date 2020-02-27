884d7c45687fff5c23e2b04e04a79e65eeba9495
NoamDev <37066741+NoamDev@users.noreply.github.com>
2020-02-27 19:55:00 +0200

    Revert "Roll fuchsia/sdk/core/linux-amd64 from QU3ft... to 94el1... (#16821)"
    This reverts commit 2fd4443bfb89d17ee0f623a838731b95fc724890.

--- shell/platform/fuchsia/runtime/dart/utils/tempfs.cc
+++ shell/platform/fuchsia/runtime/dart/utils/tempfs.cc
@@ -45,10 +45,9 @@ void RunnerTemp::Start() {
     zx_status_t status = memfs_install_at_with_page_limit(
         loop_->dispatcher(), kMaxTmpPages, kTmpPath);
 #else
-    memfs_filesystem_t* fs;
     // Hot reload uses /tmp to hold the updated dills and assets so do not
     // impose any size limitation in non product runners.
-    zx_status_t status = memfs_install_at(loop_->dispatcher(), kTmpPath, &fs);
+    zx_status_t status = memfs_install_at(loop_->dispatcher(), kTmpPath);
 #endif
     finished.set_value();
     if (status != ZX_OK) {
