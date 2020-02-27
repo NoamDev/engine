a28d75cf96ab61b1a3e31e9b45f7aabe89f5b645
NoamDev <37066741+NoamDev@users.noreply.github.com>
2020-02-27 19:55:00 +0200

    Revert "Fix and consolidate wstring conversion utils (#16342)"
    This reverts commit 2c883108e1c861c798deae9dcbb7adadc593417a.

--- fml/platform/win/wstring_conversion.h
+++ fml/platform/win/wstring_conversion.h
@@ -12,7 +12,16 @@
 namespace fml {
 
 using WideStringConvertor =
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>;
+    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t>;
+
+inline std::wstring ConvertToWString(const char* path) {
+  if (path == nullptr) {
+    return {};
+  }
+  std::string path8(path);
+  std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> wchar_conv;
+  return wchar_conv.from_bytes(path8);
+}
 
 inline std::wstring StringToWideString(const std::string& str) {
   WideStringConvertor converter;
